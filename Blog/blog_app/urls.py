21cd04c41db050654379d93b1560d77cb0f1725c
Pranay K Prince <pranaykprince@gmail.com>
2020-04-21 22:13:17 +0530

    django_part_2

--- Blog/blog_app/urls.py
+++ Blog/blog_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('blog/<slug>/',views.slug),
+    path('dashboard',views.dashboard),
 ]
--- Blog/blog_app/views.py
+++ Blog/blog_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import Blog
 
 
 #def index(request):
@@ -8,7 +8,24 @@ from django.http import HttpResponse
 
 
 def index(request):
+    blog = Blog.objects.all()
     argument={
         'name':'Pranay',
+        'blog':blog
     }
     return render(request,'index.html',argument)
+
+def slug(request,slug):
+    blog = Blog.objects.filter(slug=slug)
+    argument={
+        'name':'Pranay',
+        'blog':blog
+    }
+    return render(request,'blogtemplate.html',argument)
+
+def dashboard(request):
+    blog = Blog.objects.all()
+    argument={
+        'blog':blog
+    }
+    return render(request,'dashboard.html',argument)
